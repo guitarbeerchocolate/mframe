328ecad5ea831450d2229b985675b2ae48289303
Mick Redman <guitarbeerchocolate@googlemail.com>
2016-05-11 16:54:31 +0100

    Added matchactive

--- includes/navigation.inc.php
+++ includes/navigation.inc.php
@@ -5,7 +5,7 @@
 		</div>
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			<ul class="nav navbar-nav">
-				<li class="active"><a href="index.php">Home</a></li>
+				<li><a href="index.php">Home</a></li>
 		        <li><a href="test.php">Test</a></li>
 			</ul>
 		</div>
---
+++ modules/matchactive/matchactive.js
@@ -0,0 +1,22 @@
+var pathArray = window.location.pathname.split( '/' );
+var sn = pathArray[pathArray.length - 1];
+var setOfAnchors = $('ul.nav > li > a');
+var anchorFound = false;
+$(setOfAnchors).each(function()
+{
+	if($(this).attr('href') == sn)
+	{
+		$(this).parent().addClass('active');
+        anchorFound = true;
+	}
+});
+if(anchorFound == false)
+{
+    $(setOfAnchors).each(function()
+    {
+        if(($(this).attr('href') == 'index.php') || ($(this).attr('href') == 'index.html'))
+        {
+            $(this).parent().addClass('active');                       
+        }
+    });
+}
