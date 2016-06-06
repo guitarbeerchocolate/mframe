37a8e252c11a7a81409678b98ad0b03254937411
e101395 <guitarbeerchocolate@googlemail.com>
2016-06-06 12:52:34 +0100

    Updated .htaccess and added SEO page

--- includes/general/meta.inc.php
+++ includes/general/meta.inc.php
@@ -3,12 +3,12 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">		
 <meta name="apple-mobile-web-app-capable" content="yes" />
-<meta name="description" content="" />
+<meta name="description" content="<?php echo $settings['website']['name'].''.$cfn; ?>" />
 <meta name="google-site-verification" content="" />
 <meta name="keywords" content="" />
 <meta name="author" content="Mick Redman, mick.redman@effectivewebdesigns.co.uk">
-<meta name="dc.language" CONTENT="en">
-<meta name="dc.title" CONTENT="Effective Web Designs">
-<meta name="dc.description" CONTENT="Effective Web Designs are specialists in the design and development of websites, web applications and virtual learning environments for the education and research sectors. This is the home page.">
+<meta name="dc.language" content="en">
+<meta name="dc.title" content="Effective Web Designs">
+<meta name="dc.description" content="<?php echo $settings['website']['name'].''.$cfn; ?>">
 <meta name="author" content="Mick Redman, mick.redman@effectivewebdesigns.co.uk">
 <meta name="ROBOTS" content="INDEX, FOLLOW">
---
+++ includes/public/seo.inc.php
@@ -0,0 +1,34 @@
+<div class="row">
+	<div class="container">
+		<div class="col-md-12">
+			<h2>SEO</h2>
+			<h3>Checklist</h3>
+			Research the best keywords to use in your website to attract the users you are looking for. Then, include them in your meta tags and apply the 'strong' tag to the words as they appear in your content.&nbsp;You can use this page to help <a href="https://adwords.google.com/KeywordPlanner">https://adwords.google.com/KeywordPlanner</a><br />
+			Create a sitemap.xml for your website using <a href="https://www.xml-sitemaps.com/">https://www.xml-sitemaps.com/</a>.<br />
+			Register your website with the various search engines using:<br />
+			<ul>
+			<li><a href="http://www.google.co.uk/submityourcontent/website-owner/">http://www.google.co.uk/submityourcontent/website-owner/</a></li>
+			<li><a href="http://www.bing.com/toolbox/submit-site-url">http://www.bing.com/toolbox/submit-site-url</a></li>
+			<li><a href="http://search.yahoo.com/info/submit.html">http://search.yahoo.com/info/submit.html</a></li>
+			</ul>
+			Make sure that when a visitor clicks your website’s logo, they are taken back to the homepage.<br />
+			Optimise images using Gimp with techniques show here <a href="http://www.makeuseof.com/tag/optimize-images-web-gimp/">http://www.makeuseof.com/tag/optimize-images-web-gimp/</a><br />
+			Adding ALT tags to all of the images. If the graphic is just for decorative purposes, just use ALT=""<br />
+			Use the same colour for links throughout the website.<br />
+			Add at least one picture to every page of the website.<br />
+			Validate the source code with W3C validation tools and correct any errors using&nbsp;<a href="http://validator.w3.org/">http://validator.w3.org/</a><br />
+			Add Google analytics code to every page of the website. See <a href="http://www.google.com/analytics/">http://www.google.com/analytics/</a>.<br />
+			Create a favicon for the website. If possible also include other icons for tablets and smartphones etc. See&nbsp;<a href="http://iconifier.net/">http://iconifier.net/</a>.<br />
+			Only use email address that include @thesamedomain.<br />
+			Label each box of any forms on the website.<br />
+			Add Google site verification tag. See&nbsp;<a href="https://support.google.com/webmasters/answer/35659?hl=en">https://support.google.com/webmasters/answer/35659?hl=en</a><br />
+			Show a confirmation any time a visitor completes a significant action on the website.<br />
+			Enlist the website with web directories such as http://dir.yahoo.com/ and http://www.dmoz.org/<br />
+			Add microdata to your tags, see http://schema.org/<br />
+			Include feeds from your blogs, facebook or twitter to make pages dynamic.<br />
+			Create a Google+ page, see <a href="https://plus.google.com/pages/create">https://plus.google.com/pages/create</a>.<br />			
+			Create a custom 404 error page, see&nbsp;<a href="http://www.thesitewizard.com/archive/custom404.shtml">http://www.thesitewizard.com/archive/custom404.shtml</a>.<br />
+			Add Dublin Core meta elements, see&nbsp;<a href="http://www.metatags.info/dublin_core_metadata_element_set">http://www.metatags.info/dublin_core_metadata_element_set</a>
+		</div>
+	</div>
+</div>
