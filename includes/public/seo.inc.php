e0a7c7ad9a3d1a68b1135f8ea24301bb1aee3fe9
Mick Redman <guitarbeerchocolate@googlemail.com>
2016-06-08 16:45:33 +0100

    SEO include updated

--- includes/public/seo.inc.php
+++ includes/public/seo.inc.php
@@ -3,15 +3,22 @@
 		<div class="col-md-12">
 			<h2>SEO</h2>
 			<ul class="nav nav-tabs" role="tablist">
-				<li role="presentation" class="active"><a href="#checklist" aria-controls="checklist" role="tab" data-toggle="tab">Checklist</a></li>
-				<li role="presentation"><a href="#local" aria-controls="local" role="tab" data-toggle="tab">Local</a></li>
+				<li role="presentation" class="active"><a href="#basics" aria-controls="basics" role="tab" data-toggle="tab">Basics</a></li>
+				<li role="presentation"><a href="#technical" aria-controls="technical" role="tab" data-toggle="tab">Technical</a></li>
+				<li role="presentation"><a href="#content" aria-controls="content" role="tab" data-toggle="tab">Content</a></li>				
 				<li role="presentation"><a href="#linkbuilding" aria-controls="linkbuilding" role="tab" data-toggle="tab">Link building</a></li>
+				<li role="presentation"><a href="#local" aria-controls="local" role="tab" data-toggle="tab">Local</a></li>
 			</ul>
 			<div class="tab-content">
-        		<div role="tabpanel" class="tab-pane active" id="checklist">
-        			<p>
-        				Research the best keywords to use in your website to attract the users you are looking for. Then, include them in your meta tags and apply the 'strong' tag to the words as they appear in your content.&nbsp;You can use this page to help <a href="https://adwords.google.com/KeywordPlanner">https://adwords.google.com/KeywordPlanner</a>.
-        			</p>
+				<div role="tabpanel" class="tab-pane active" id="basics">
+					<p>SEO can be divided into 3 main processes : </p>
+					<ul>
+						<li>Technical - The work done by the techie so that the site performs well with search engines.</li>
+						<li>Content - The work done to maximise key phrases picked up by the search engines, but also make the words meaningful to the user.</li>
+						<li>Link building - The process of getting content from other (preferably high quality) websites pointing at yours.</li>
+					</ul>
+				</div>
+        		<div role="tabpanel" class="tab-pane" id="technical">
 					<p>Create a sitemap.xml for your website using <a href="https://www.xml-sitemaps.com/">https://www.xml-sitemaps.com/</a>.</p>
 					<p>Register your website with the various search engines using:</p>
 					<ul>
@@ -20,35 +27,39 @@
 						<li><a href="http://search.yahoo.com/info/submit.html">http://search.yahoo.com/info/submit.html</a></li>
 					</ul>
 					<p>Make sure that when a visitor clicks your website’s logo, they are taken back to the homepage.</p>
-					<p>
-						Optimise images using Gimp with techniques show here <a href="http://www.makeuseof.com/tag/optimize-images-web-gimp/">http://www.makeuseof.com/tag/optimize-images-web-gimp/</a>
-					</p>
+					<p>Optimise images using Gimp with techniques show here <a href="http://www.makeuseof.com/tag/optimize-images-web-gimp/">http://www.makeuseof.com/tag/optimize-images-web-gimp/</a></p>
 					<p>Adding ALT tags to all of the images. If the graphic is just for decorative purposes, just use ALT="".</p>
 					<p>Use the same colour for links throughout the website.</p>
-					<p>Add at least one picture to every page of the website.</p>
-					<p>
-						Validate the source code with W3C validation tools and correct any errors using&nbsp;<a href="http://validator.w3.org/">http://validator.w3.org/</a>
-					</p>
+					<p>Validate the source code with W3C validation tools and correct any errors using&nbsp;<a href="http://validator.w3.org/">http://validator.w3.org/</a></p>
 					<p>Add Google analytics code to every page of the website. See <a href="http://www.google.com/analytics/">http://www.google.com/analytics/</a>.</p>
 					<p>Create a favicon for the website. If possible also include other icons for tablets and smartphones etc. See&nbsp;<a href="http://iconifier.net/">http://iconifier.net/</a>.</p>
 					<p>Only use email address that include @thesamedomain.</p>
 					<p>Label each box of any forms on the website.</p>
-					<p>
-						Add Google site verification tag. See&nbsp;<a href="https://support.google.com/webmasters/answer/35659?hl=en">https://support.google.com/webmasters/answer/35659?hl=en</a>
-					</p><br />
+					<p>Add Google site verification tag. See&nbsp;<a href="https://support.google.com/webmasters/answer/35659?hl=en">https://support.google.com/webmasters/answer/35659?hl=en</a></p>
 					<p>Show a confirmation any time a visitor completes a significant action on the website.</p>
 					<p>Enlist the website with web directories such as http://dir.yahoo.com/ and http://www.dmoz.org/.</p>
 					<p>Add microdata to your tags, see http://schema.org/</p>
 					<p>Include feeds from your blogs, facebook or twitter to make pages dynamic.</p>
 					<p>Create a Google+ page, see <a href="https://plus.google.com/pages/create">https://plus.google.com/pages/create</a>.</p>
 					<p>Create a custom 404 error page, see&nbsp;<a href="http://www.thesitewizard.com/archive/custom404.shtml">http://www.thesitewizard.com/archive/custom404.shtml</a>.</p>
-					<p>
-						Add Dublin Core meta elements, see&nbsp;<a href="http://www.metatags.info/dublin_core_metadata_element_set">http://www.metatags.info/dublin_core_metadata_element_set</a>
-					</p>
+					<p>Add Dublin Core meta elements, see&nbsp;<a href="http://www.metatags.info/dublin_core_metadata_element_set">http://www.metatags.info/dublin_core_metadata_element_set</a></p>
+					<p>Wherever possible use HTML5 tags such as article, section, figure, aside, etc.</p>
+					<p>Try add headers to article tags.</p>
+					<p>Make your pages as fast as possible. Use a tool like <a href="https://developers.google.com/speed/pagespeed/insights/">https://developers.google.com/speed/pagespeed/insights/</a> to improve this.</p>
+        		</div>
+        		<div role="tabpanel" class="tab-pane" id="content">
+        			<p>Get a handle on your audience. Who are they? Segment the market a little.</p>
+        			<p>Build a list of phrases which could be used to attract this audience.</p>
+        			<p>Use a tool like <a href="https://ubersuggest.io/">ubersuggest.io/</a> to get variant on those phrases.</p>
+        			<p>Research how well those keywords attract the users you are looking for. Then, include them in your meta tags and apply the 'strong' tag to the words as they appear in your content. You can use this page to help <a href="https://adwords.google.com/KeywordPlanner">https://adwords.google.com/KeywordPlanner</a>.</p>
+        			<p>Search for competitor hits on those phrases.</p>
+        			<p>See if your competitors are missing anything in their phrases and exploit those gaps.</p>
+        			<p>Add at least one picture to every page of the website.</p>
+        		</div>        		
+        		<div role="tabpanel" class="tab-pane" id="linkbuilding">
+        			<p>Create a social media account. Whenever it's possible, include a link to the website in the comments.</p>
         		</div>
         		<div role="tabpanel" class="tab-pane" id="local">
-        		</div>
-        		<div role="tabpanel" class="tab-pane" id="linkbuilding">
         		</div>
         	</div>
 		</div>
