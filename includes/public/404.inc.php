d3afc331ebf6c901adeb93f2a8f8ed1085aac10e
Mick Redman <guitarbeerchocolate@googlemail.com>
2016-09-07 14:42:14 +0100

    Added 404

---
+++ includes/public/404.inc.php
@@ -0,0 +1,13 @@
+<div class="row">
+	<div class="container">
+		<div class="col-md-12">
+			<h2>Error 404</h2>
+      <p>The page you requested</p>
+      <script type="text/javascript">
+      document.write('<a href="'+document.referrer+'" target="_blank">'+document.referrer+'</a>');
+      </script>
+			<p>does not exist.</p>
+			<p>Please return to the <a href="index.php">home page</a>.</p>
+	</div>
+	</div><!-- .container -->
+</div><!-- .row -->
