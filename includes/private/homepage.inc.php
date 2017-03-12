d67f358e83a58547a8716986dd493fd341f544e9
Mick Redman <guitarbeerchocolate@googlemail.com>
2017-03-12 17:01:55 +0000

    Just checking

--- includes/private/homepage.inc.php
+++ includes/private/homepage.inc.php
@@ -1,6 +1,8 @@
 <?php
 $h3 = $bs->tag('h3','Your profile');
 $row = $db->getOneByFieldValue('profiles','userid',$s->userid);
+$h4 = NULL;
+$img = NULL;
 if($row == TRUE)
 {
 	$name = $row['name'];
@@ -20,7 +22,7 @@ else
 	$photo = '';
 	$action = 'profiles/addprofiles';
 }
-$bs->singleRow(NULL, $h4.$content.$img);
+$bs->singleRow(NULL, $h3.$h4.$content.$img);
 $bs->render();
 include 'sub/profileform.inc.php';
 ?>
