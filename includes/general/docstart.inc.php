1177ed2acbf722b64fcb62b23aec159169641365
Mick Redman <guitarbeerchocolate@googlemail.com>
2017-03-30 13:19:51 +0100

    Improved social media in docstart

--- includes/general/docstart.inc.php
+++ includes/general/docstart.inc.php
@@ -2,11 +2,11 @@
 <?php
 if(isset($liveConfig['id']))
 {
-    echo '<html lang="en" prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#" itemscope itemtype="http://schema.org/Article">';
+    echo '<html lang="en-GB" prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#" itemscope itemtype="http://schema.org/Article">';
 }
 else
 {
-        echo '<html lang="en" itemscope itemtype="http://schema.org/Organization">';
+        echo '<html xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml" xmlns:website="http://ogp.me/ns/website" lang="en-GB" itemscope itemtype="http://schema.org/WebPage">';
 }
 ?>
 <head>
