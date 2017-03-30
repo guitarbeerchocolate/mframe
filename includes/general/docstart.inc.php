8d7714b02209d44cbdf47e99b7634fc3aa5ad89f
Mick Redman <guitarbeerchocolate@googlemail.com>
2017-03-30 12:30:29 +0100

    Added improvements in meta data

--- includes/general/docstart.inc.php
+++ includes/general/docstart.inc.php
@@ -2,7 +2,7 @@
 <?php
 if(isset($liveConfig['id']))
 {
-    echo '<html lang="en" itemscope itemtype="http://schema.org/Article">';
+    echo '<html lang="en" prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#" itemscope itemtype="http://schema.org/Article">';
 }
 else
 {
